d81e736f28ecc85e27025cbc2e80893d7098184b
wysvince <43325628+wysvince@users.noreply.github.com>
2018-11-21 14:39:47 -0700

    update ReadInputFile files
    added checkDuplicatedVariables()

--- hlsyn/hlsyn/WriteOutputFile.h
+++ hlsyn/hlsyn/WriteOutputFile.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef WRITEOUTPUTFILE_H
+#define WRITEOUTPUTFILE_H
+
+class WriteOutputFile {
+public:
+	// fields
+
+	// constructor
+	WriteOutputFile();
+
+	// setters
+
+	// getters
+
+	// methods
+	void createHLSMHeader(std::ofstream &file);
+	void createDeclarations(std::ofstream &file, ReadInputFile &read);
+};
+
+#endif // !WRITEOUTPUTFILE_H
