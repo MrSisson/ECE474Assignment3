5cfe9fad7a0a0a6550502d31ba7b5c1637e7b9c7
wysvince <43325628+wysvince@users.noreply.github.com>
2018-12-01 12:04:58 -0700

    New Source Files
    going back to assignment2 source files

---
+++ new_source_files/Graph.cpp
@@ -0,0 +1,57 @@
+#include "Graph.h"
+
+using namespace std;
+
+// constructors
+Graph::Graph() {} // default constructor
+
+Graph::Graph(Nodes newNodes, Edges newEdges, int newWeight) {
+	this->nodes.push_back(newNodes);
+	this->edges.push_back(newEdges);
+	this->weight = newWeight;
+}
+
+// setters
+void Graph::setNodes(Nodes newNodes) {
+	this->nodes.push_back(newNodes);
+}
+
+void Graph::setEdges(Edges newEdges) {
+	this->edges.push_back(newEdges);
+}
+
+void Graph::setWeight(int newWeight) {
+	this->weight = newWeight;
+}
+
+// getters
+vector<Nodes> Graph::getNodes() {
+	return this->nodes;
+}
+
+vector<Edges> Graph::getEdges() {
+	return this->edges;
+}
+
+int Graph::getWeight() {
+	return this->weight;
+}
+
+void Graph::calculateWeights(Operations &op)
+{
+	int tempWeight = -1;
+
+	// Calculate weight by determining type of operation
+
+	this->weight = tempWeight;
+}
+
+void Graph::setupNodesEdges(ReadInputFile &rif)
+{	
+	int tempStateNum = 0;
+	int tempNumCycles = 0;
+	string tempOperation = "\0";
+	for (Operations op : rif.operationList) {
+		this->nodes.
+	}
+}
